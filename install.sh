215f7476184e620f015f78a8aad41bd2edec3557
Daniel TePaske <daniel@okcupid.com>
2021-04-29 14:57:18 -0400

    add credit

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # install package manually since it doesn't appear in the Atom package list
+# with thanks to https://discuss.atom.io/t/manually-install-package/9251/21
 apm install
 apm link
